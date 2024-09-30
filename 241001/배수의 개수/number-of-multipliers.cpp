3efc3160efffd97a5fabcf81c0d8c0d5fb48f49c
박서진 <kevin8193@naver.com>
2024-10-01 00:38:03 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241001/배수의 개수/number-of-multipliers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int cnt3, cnt5;
+
+    for (int i = 10; i < 10; i++) {
+        int a;
+        std::cin >> a;
+
+        if (a % 3 == 0) {
+            cnt3 += 1;
+        }
+        if (a % 5 == 0) {
+            cnt5 += 1;
+        }
+
+        std::cout << cnt3 << " " << cnt5;
+    }
+
+    return 0;
+}
