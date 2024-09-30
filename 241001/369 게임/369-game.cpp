a2c3a691e1b145b9df0d31ec5fe48dac8c518449
박서진 <kevin8193@naver.com>
2024-10-01 00:18:10 +0900

    Title: 369 게임, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241001/369 게임/369-game.cpp
+++ 241001/369 게임/369-game.cpp
@@ -4,49 +4,38 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n;
-
     std::cin >> n;
 
     for (int i = 1; i <= n; i++) {
-        if(i < 10) {
-            if (i%3==0) {
+        if (i < 3) {
+            std::cout << i << " ";
+        }
+        else if (i >= 3 && i < 30) {
+            if(i % 3 == 0) {
+                std::cout << 0 << " ";
+            }
+            else if((i % 10) % 3 == 0) {
                 std::cout << 0 << " ";
             }
             else {
                 std::cout << i << " ";
             }
         }
-        else if (i >= 30 && i < 100) {
-            int t10, t1;
-            t10 = i /10;
-            t1 = i % 10;
+        else {
+            int t10,t1;
 
-            if(i%3 == 0) {
-                std::cout << 0 << " ";
-            }
-            else if (t1 % 3 == 0 || t10 % 3 == 0) {
-                std::cout << 0 << " ";
-            }
-            else {
-                std::cout << 0 << " ";
-            }
-        }
-        else if (i >= 10 && i < 30) {
-            int t10, t1;
             t10 = i / 10;
             t1 = i % 10;
-            
-            
-            if (i%3 == 0) {
+
+            if (i % 3 == 0) {
                 std::cout << 0 << " ";
             }
-            else if (t1 % 3 == 0){
+            else if(t10 % 3 == 0 || t1 % 3 == 0) {
                 std::cout << 0 << " ";
             }
             else {
                 std::cout << i << " ";
             }
-            
         }
     }
 
