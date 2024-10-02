e153acc32c84ffbababa1772062102715b097178
박서진 <kevin8193@naver.com>
2024-10-02 11:13:57 +0900

    Title: ab 사이에 있는 c, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241002/ab 사이에 있는 c/c-between-a-and-b.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    bool flag = false;
+
+    std::cin >> a >> b >> c;
+
+    for (int i = a; i <= b; i++) {
+        if (i % c == 0) {
+            flag = true;
+        }
+    }
+    
+    if (flag){
+        std::cout << "YES";
+    }
+    else {
+        std::cou << "NO";
+    }
+
+    return 0;
+}
