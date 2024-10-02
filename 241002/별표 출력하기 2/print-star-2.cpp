b1e3ba40c99060d0b1a2423c82f46a1e9928dcf7
박서진 <kevin8193@naver.com>
2024-10-02 11:36:19 +0900

    Title: 별표 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241002/별표 출력하기 2/print-star-2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    std::cin >> n;
+
+    for (int i = n; i > 0; i--){
+        for (int j = i; j > 0; j--){
+            std::cout << "*" << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
