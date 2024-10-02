93d74a45d649d3931237e878376477e2d0da178c
박서진 <kevin8193@naver.com>
2024-10-02 11:15:53 +0900

    Title: 합성수 판별기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241002/합성수 판별기/factorization-discriminator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    std::cin >> n;
+
+    bool flag = false;
+
+    if (int i = 2; i <= n-1; i++){
+        if (n % i == 0) {
+            flag = true;
+        }
+        else {
+            flag = false;
+        }
+    }
+
+    if (flag) {
+        std::cout << 'C';
+    }
+    else{
+        std::cout << 'N';
+    }
+
+    return 0;
+}
