453d766ecfe88c94a919527fa4334b3787debb5c
박서진 <kevin8193@naver.com>
2024-10-03 20:54:11 +0900

    Title: 1-9 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/1-9 정사각형/1-9-square.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    int cnt = 0;
+    std::cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cnt++;
+            if (cnt > 9) {
+                cnt = 1;
+            }
+
+            std::cout << cnt;
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
