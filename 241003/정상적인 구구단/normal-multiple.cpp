f9ddb6ca9c3848a1825cc7e0081cfe512efc3944
박서진 <kevin8193@naver.com>
2024-10-03 23:25:45 +0900

    Title: 정상적인 구구단, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/정상적인 구구단/normal-multiple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    std::cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++) {
+            std::cout << i << " * " << j << " = " << i * j;
+            if (j < n) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
