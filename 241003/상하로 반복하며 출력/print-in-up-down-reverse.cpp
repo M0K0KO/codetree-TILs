726c08529e610efe3cb3a0a9871428d8342447a2
박서진 <kevin8193@naver.com>
2024-10-03 22:17:22 +0900

    Title: 상하로 반복하며 출력, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241003/상하로 반복하며 출력/print-in-up-down-reverse.cpp
+++ 241003/상하로 반복하며 출력/print-in-up-down-reverse.cpp
@@ -6,6 +6,10 @@ int main() {
     int n;
     std::cin >> n;
 
+    if (n == 1) {
+        std::cout << 1;
+    }
+    
     for (int i = 1; i <= n; i++){
         if (i % 2 == 1){
             for (int j = 0 ; j < n / 2; j++) {
