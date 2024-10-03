8702bfe8e1262d9cf31652acd8a0cd76c6de916f
박서진 <kevin8193@naver.com>
2024-10-03 20:38:09 +0900

    Title: 행과 열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/행과 열/row-column.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+
+    std::cin >> a >> b;
+
+    for (int i = 1; i <= a; i++){
+        for (int j = 1; j <= b; j++){
+            std::cout << i * j << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
