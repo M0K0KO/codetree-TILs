c204377443c44d2e3acd78254700dd1a9b3816de
박서진 <kevin8193@naver.com>
2024-10-04 13:04:19 +0900

    Title: 배열 채우기, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 241004/배열 채우기/filling-array.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+
+    int a;
+    int i = 0;
+
+    do {
+        std::cin >> a;
+        if (a == 0)
+            break;
+
+        arr[i] = a;
+        i++;
+    } while(true);
+
+    for (int j = i-1; j >= 0; j--){
+        std::cout << arr[j] << " ";
+    }
+
+    return 0;
+}
