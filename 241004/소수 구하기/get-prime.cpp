49aaf7529569734dedd82f102cd6c922fb9d820f
박서진 <kevin8193@naver.com>
2024-10-04 12:39:26 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/소수 구하기/get-prime.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    std::cin >> n;
+
+
+    std::cout << 2 << " ";
+
+    for (int i = 2; i <= n; i++){
+        for (int j = 2; j < n; j++){
+            if (i % j == 0){
+                break;
+            }
+
+            if (j == n-1) {
+                std::cout << i << " ";
+            }
+        }
+    }
+
+    return 0;
+}
