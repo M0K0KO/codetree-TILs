4362d26a369e1e4b2f1c1094a8c90e8896d2c06d
박서진 <kevin8193@naver.com>
2024-10-04 12:42:58 +0900

    Title: 소수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241004/소수 구하기/get-prime.cpp
+++ 241004/소수 구하기/get-prime.cpp
@@ -8,7 +8,7 @@ int main() {
 
     for (int i = 2; i <= n; i++){
         int cnt = 0;
-        for (int j = 2; i < i; j++){
+        for (int j = 2; j < i; j++){
             if (i % j == 0){
                 cnt++;
             }
