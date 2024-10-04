0f229b07a79ae290e9fc695f4eb802644feb5e02
박서진 <kevin8193@naver.com>
2024-10-04 12:48:41 +0900

    Title: 배열 채우고 출력하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/배열 채우고 출력하기/filling-array-and-print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[10];
+
+    for (int i = 0; i < 10; i++){
+        std::cin >> arr[i];
+    }
+
+    for (int i = 0; i < 10; i++){
+        std::cout << arr[9-i];
+    }
+
+    return 0;
+}
