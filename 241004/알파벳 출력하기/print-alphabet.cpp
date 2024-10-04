d4d7dcd989b86c4d44122edb449de5ba0c4b26f6
박서진 <kevin8193@naver.com>
2024-10-04 12:21:56 +0900

    Title: 알파벳 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/알파벳 출력하기/print-alphabet.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    std::cin >> n;
+
+    int cnt = 64;
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0 ; j <= i; j++){
+            cnt++;
+            if (cnt == 91) {
+                cnt = 65;
+            }
+            std::cout << char(cnt);
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
+}
