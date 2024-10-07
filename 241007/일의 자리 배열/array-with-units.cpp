3b9a977888fc2f2d902fe833c97f538ec3c6cb26
박서진 <kevin8193@naver.com>
2024-10-07 10:48:03 +0900

    Title: 일의 자리 배열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241007/일의 자리 배열/array-with-units.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int arr[10];
+
+    std::cin >> arr[0] >> arr[1];
+
+    for (int i = 2; i < 10; i++){
+        arr[i] = (arr[i-1] + arr[i-2]) % 10;
+    }
+
+    for (int i = 0; i < 10; i++){
+        std::cout << arr[i] << " ";
+    }
+
+    return 0;
+}
